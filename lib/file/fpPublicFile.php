d964b59a9da0b4d4261374a63a5b8efafd05c9aa
Maksim Kotlyar <maksim@maksim-pc.(none)>
2010-08-27 18:51:53 +0300

    fpPublicFile. ability to build absolute url

--- lib/file/fpPublicFile.php
+++ lib/file/fpPublicFile.php
@@ -14,13 +14,24 @@ class fpPublicFile extends fpFile
   /**
    * @return string
    */
-  public function getUrl()
+  public function getUrl($absolute = false)
   {
     if (strpos($this->getPath(), sfConfig::get('sf_web_dir')) === false) {      
       throw new fpFileException('The file `'.$this->getPath().'` is not a valid public file. The file should be in sub subdirectory of `'.sfConfig::get('sf_web_dir').'`');
     }
     
-    return str_replace(sfConfig::get('sf_web_dir'), '', $this->getPath());
+    $domain = '';
+    if ($absolute)  {
+      if (!isset($_SERVER['HTTP_HOST'])) {
+        throw new Exception('The $_SERVER[\'HTTP_HOST\'] is not defined so I cannot guess what is the domain. Maybe you trying to use it from CLI?');
+      }
+      
+      $domain = 'http://'.$_SERVER['HTTP_HOST'];
+    }
+    
+    $relativeUrl = str_replace(sfConfig::get('sf_web_dir'), '', $this->getPath()); 
+    
+    return $domain . $relativeUrl;
   }
   
   /**
